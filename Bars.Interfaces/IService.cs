030ff48ff2e35860f4aa58706c14efe9b7b9bce3
Marat <mgfan@yandex.ru>
2022-06-13 10:36:49 +0400

    Implemented Bars.Interfaces project

---
+++ Bars.Interfaces/IService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+using Bars.Entities.Dto;
+
+namespace Bars.Interfaces
+{
+    [ServiceContract]
+    public interface IServiceContracts : IService
+    {
+        [OperationContract]
+        public List<Contract> GetContracts();
+    }
+}
